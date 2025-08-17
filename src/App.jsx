7c500179fc6ba9afda56781c9b1f502b2ad4b72d
hema1124-11 <hemapriyaaa1124@gmail.com>
2025-08-17 12:53:46 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./pages/Navbar";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./pages/Home";
 import Home from "./pages/Home";
-import WatchList from "./pages/WatchList";
+import WatchList from "./pages/WatchList"
 import { WatchListProvider } from "./context/WatchListContext";
 
 export const api_key = "676612bb00a0e3cbd7c65ff9c8c6ef92";
@@ -9,13 +9,13 @@ export const api_key = "676612bb00a0e3cbd7c65ff9c8c6ef92";
 function App() {
   return (
     <WatchListProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/watchlist" element={<WatchList />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </WatchListProvider>
   );
 }
